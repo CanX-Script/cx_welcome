9bdeb2b7bd62cbd0d1c848df7c216c724d3a47d4
Hosein <48409432+Ho3einWave@users.noreply.github.com>
2023-09-26 12:16:36 +0330

    frontend done

--- client/client.lua
+++ client/client.lua
@@ -17,9 +17,55 @@ end)
 RegisterNUICallback('getClientData', function(data, cb)
   debugPrint('Data sent by React', json.encode(data))
 
--- Lets send back client coords to the React frame for use
+  -- Lets send back client coords to the React frame for use
   local curCoords = GetEntityCoords(PlayerPedId())
 
   local retData <const> = { x = curCoords.x, y = curCoords.y, z = curCoords.z }
   cb(retData)
+end)
+
+
+local pedCoords = vector4(219, -815, 29.6, 155)
+local pedModel = GetHashKey("a_m_m_business_01")
+RequestModel(pedModel)
+
+while not HasModelLoaded(pedModel) do
+  Citizen.Wait(0)
+end
+
+local ped = CreatePed(4, pedModel, pedCoords, true, true, true)
+
+SetBlockingOfNonTemporaryEvents(ped, true)
+SetPedCanPlayAmbientAnims(ped, true)
+SetEntityInvincible(ped, true)
+FreezeEntityPosition(ped, true)
+
+local shown = false
+local inDistance = false
+
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(0)
+    inDistance = false
+    local coords = GetEntityCoords(PlayerPedId())
+
+
+    if GetDistanceBetweenCoords(coords, pedCoords, true) < 5 then
+      inDistance = true
+      if IsControlJustReleased(0, 38) then
+        toggleNuiFrame(true)
+      end
+    else
+      inDistance = false
+      -- Your Code Goes Here
+    end
+
+    if not shown and inDistance then
+      ESX.TextUI("Press ~b~[E]~s~", "info")
+      shown = true
+    elseif shown and not inDistance then
+      ESX.HideUI()
+      shown = false
+    end
+  end
 end)
--- web/src/utils/misc.ts
+++ web/src/utils/misc.ts
@@ -1,6 +1,12 @@
 // Will return whether the current environment is in a regular browser
 // and not CEF
-export const isEnvBrowser = (): boolean => !(window as any).invokeNative
+export const isEnvBrowser = (): boolean => !(window as any).invokeNative;
 
 // Basic no operation function
-export const noop = () => {}
+export const noop = () => {};
+
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
